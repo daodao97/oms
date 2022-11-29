df4233ca95090eeed37dd32c0bdb4654399e546d
okiss <okissjs@gmail.com>
2022-11-29 16:22:24 +0800

    up

--- build/preinstall.js
+++ build/preinstall.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const r = (...args) => path.resolve(__dirname, ...args)
+
 const getDirectories = source =>
     fs.readdirSync(source, { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
@@ -16,13 +18,13 @@ function savePkgJson(dir, data) {
     fs.writeFileSync(path.join(dir, 'package.json'), str)
 }
 
-const base = path.resolve(__dirname, '../')
+const base = r('../')
 const basePkg = pkgJson(base)
 
 const pkgs = getDirectories(path.join(__dirname, '../packages'))
 
 pkgs.forEach(pkgName => {
-    let ROOT = path.resolve(__dirname, '../packages/' + pkgName)
+    let ROOT = r(__dirname, '../packages/' + pkgName)
     const pkg = pkgJson(ROOT)
     Object.keys(pkg.dependencies).forEach(key => {
         if (basePkg.dependencies[key] !== undefined) {
