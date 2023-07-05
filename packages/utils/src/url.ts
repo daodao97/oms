2084fe0731a6052e1d04b139dd7c6974c5d39dae
daodao97 <daodao97@foxmail.com>
2023-07-05 14:44:32 +0800

    up

--- packages/utils/src/url.ts
+++ packages/utils/src/url.ts
@@ -1,5 +1,58 @@
-// @ts-ignore
-import buildURL from 'axios/lib/helpers/buildURL'
+import { isDate, isObject } from 'lodash'
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any) {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach((key) => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values: string[]
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach((val) => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  const serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
+
 export function queryParams(str: string) {
   const queryString = str.indexOf('?') !== -1 ? str.slice(str.indexOf('?') + 1) : (str.indexOf('/') !== -1 ? '' : str)
   const pairs = queryString.split('&')
