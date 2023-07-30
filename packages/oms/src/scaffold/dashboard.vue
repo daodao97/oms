d301986c2384af59c4d962474d29194d300ba0ca
daodao97 <daodao97@foxmail.com>
2023-07-30 10:22:46 +0800

    up

--- packages/oms/src/scaffold/dashboard.vue
+++ packages/oms/src/scaffold/dashboard.vue
@@ -2,24 +2,21 @@
 import { useStore } from 'vuex'
 import { getCmp } from '../utils/container'
 import { computed } from 'vue'
-import EmptyRole from './EmptyRole.vue'
 
 export default defineComponent({
   name: 'Dashboard',
   setup() {
     const store = useStore()
-    const roleIds = computed(() => store.state.settings.loginTips)
+    // const roleIds = computed(() => store.state.settings.loginTips)
     const custom = getCmp('dashboard')
     if (custom) {
       return () => <span>
         <custom/>
-        { roleIds.value.length === 0 ? <EmptyRole/> : undefined }
       </span>
     }
     const name = computed(() => store.getters.name)
     return () => <div class='dashboard-container'>
       <div class='dashboard-text'>Hi { name.value } 😄</div>
-      { roleIds.value.length === 0 ? <EmptyRole/> : undefined }
     </div>
   }
 })
